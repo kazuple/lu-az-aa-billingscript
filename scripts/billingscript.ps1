0256dffa6b573ce89c2efa345f3e9ba60e3e7b36
kazuple <kazuple@gmail.com>
2022-01-27 09:38:28 +0000

    get update

--- scripts/billingscript.ps1
+++ scripts/billingscript.ps1
@@ -3,7 +3,7 @@
 
 $url = "https://prod-166.westus.logic.azure.com:443/workflows/7c0497e062234ff2ae617fd21231e89b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1eveUH7pqDXn0lrY9dspfuMXBeTIwPgrMAynFPP-5Lo"
 
-$luctUser = Get-CsOnlineUser | where-object {$_.enabled -like '*True*'} | Select-Object TenantId,DisplayName,LineURI,OnPremLineURI,OnlineVoiceRoutingPolicy
+$luctUser = Get-CsOnlineUser | where-object {$_.EnterpriseVoiceEnabled -like '*True*'} | Select-Object TenantId,DisplayName,LineURI,OnPremLineURI,OnlineVoiceRoutingPolicy
 #$luctUser = Get-CsOnlineUser -identity adelev@M365x643811.onmicrosoft.com | Select-Object DisplayName,LineURI,OnPremLineURI,OnlineVoiceRoutingPolicy
 
 #$luctApplication = Get-CsOnlineApplicationInstance | Select-Object PhoneNumber
