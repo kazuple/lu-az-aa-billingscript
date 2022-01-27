c51f44ebc238c355eb7a9614123dd381e8b328f4
kazuple <kazuple@gmail.com>
2022-01-27 13:51:02 +0000

    update

--- scripts/billingscript.ps1
+++ scripts/billingscript.ps1
@@ -1,3 +1,6 @@
+#Install Microsoft Teams PowerShell Module
+Install-Module -Name MicrosoftTeams 
+Import-Module MicrosoftTeams
 
 #Get Automation Account Variables (LUCT TLD)
 $luServiceAccountUsername = Get-AutomationVariable -Name 'luServiceAccountUsername'
