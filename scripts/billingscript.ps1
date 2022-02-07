c7115f0af45c9730ed68641125b1df579a50d00a
kazuple <kazuple@gmail.com>
2022-02-07 17:25:24 +0000

    update

--- scripts/billingscript.ps1
+++ scripts/billingscript.ps1
@@ -11,9 +11,14 @@ $PWord = ConvertTo-SecureString -String $luServiceAccountPassword -AsPlainText -
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 Connect-MicrosoftTeams -Credential $credential
 
+#First/Last Day of Month
+$CURRENTDATE=GET-DATE -Format "MM/dd/yyyy"
+$FIRSTDAYOFMONTH=GET-DATE $CURRENTDATE -Day 1
+$LASTDAYOFMONTH=GET-DATE $FIRSTDAYOFMONTH.AddMonths(1).AddSeconds(-1)
+
 $tenantId = (Get-CsTenant).VerifiedDomains.Name | Where-Object {$_ -match "^([^.]+).onmicrosoft.com"}
-$luctUser = Get-CsOnlineUser | where-object {$_.EnterpriseVoiceEnabled -like '*True*' -and ($_.Enabled -like '*True')} | Select-Object TenantId,DisplayName,LineURI,OnPremLineURI,OnlineVoiceRoutingPolicy
- 
-$output = ConvertTo-Json $luctUser, $tenantId
+$luctUser = Get-CsOnlineUser | where-object {$_.EnterpriseVoiceEnabled -like '*True*' -and ($_.Enabled -like '*True')} | Select-Object TenantId,DisplayName,LineURI,OnPremLineURI,OnlineVoiceRoutingPolicy, @{Name='TenantName'; Expression = {$tenantId}}, @{Name='StartDate'; Expression = {$FIRSTDAYOFMONTH}}, @{Name='StartDate'; Expression = {$LASTDAYOFMONTH}}
+
+$output = ConvertTo-Json $luctUser
 
 Invoke-RestMethod -Method Post -Uri $endpointUrl -Body $output -ContentType application/json
