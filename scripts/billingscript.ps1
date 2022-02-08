90b82a3c76bc3feda865b4711e5598703f2c6169
kazuple <kazuple@gmail.com>
2022-02-08 11:37:33 +0000

    update

--- scripts/billingscript.ps1
+++ scripts/billingscript.ps1
@@ -14,7 +14,7 @@ $endpointUrl = "https://prod-166.westus.logic.azure.com:443/workflows/7c0497e062
 #Teams PowerShell Authentication
 $credentials = Get-AutomationPSCredential -Name 'LoopUp Service Account Credentials'
 Connect-MicrosoftTeams -Credential $credentials 
-
+Write-Output "1/4 - Connected to Teams Tenant via PowerShell"
 
 #First/Last Day of Month
 $CURRENTDATE=GET-DATE -Format "MM/dd/yyyy"
@@ -24,8 +24,15 @@ $LASTDAYOFMONTH=GET-DATE $FIRSTDAYOFMONTH.AddMonths(1).AddSeconds(-1)
 #Teams data collection
 $tenantId = (Get-CsTenant).VerifiedDomains.Name | Where-Object {$_ -match "^([^.]+).onmicrosoft.com"}
 $luctUser = Get-CsOnlineUser | where-object {$_.EnterpriseVoiceEnabled -like '*True*' -and ($_.Enabled -like '*True')} | Select-Object TenantId,DisplayName,LineURI,OnPremLineURI,OnlineVoiceRoutingPolicy, @{Name='TenantName'; Expression = {$tenantId}}, @{Name='StartDate'; Expression = {$FIRSTDAYOFMONTH}}, @{Name='StartDate'; Expression = {$LASTDAYOFMONTH}}
+Write-Output "2/4 - Collected Billing Data"
 
 #Convert data to Json
 $output = ConvertTo-Json $luctUser
 
+#Send data to endpoint url
 Invoke-RestMethod -Method Post -Uri $endpointUrl -Body $output -ContentType application/json
+Write-Output "3/4 - Data sent to LoopUp"
+
+#Remove PS Session
+Get-PSSession | Where-Object {$_.ComputerName -like "*api*"} | Remove-PSSession
+Write-Output "4/4 - PowerShell Session Removed"
