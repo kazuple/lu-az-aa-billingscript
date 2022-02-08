f5dc3a4dbea0b59b324a17fe4c3d5f6ce8d33df9
kazuple <kazuple@gmail.com>
2022-02-08 11:18:17 +0000

    update

--- scripts/billingscript.ps1
+++ scripts/billingscript.ps1
@@ -1,15 +1,20 @@
 #Get Automation Account Variables (LUCT TLD)
-$luServiceAccountUsername = Get-AutomationVariable -Name 'luServiceAccountUsername'
-$luServiceAccountPassword = Get-AutomationVariable -Name 'luServiceAccountPassword'
+#$luServiceAccountUsername = Get-AutomationVariable -Name 'luServiceAccountUsername'
+#$luServiceAccountPassword = Get-AutomationVariable -Name 'luServiceAccountPassword'
 
 #LoopUp Endpoint URL
 $endpointUrl = "https://prod-166.westus.logic.azure.com:443/workflows/7c0497e062234ff2ae617fd21231e89b/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1eveUH7pqDXn0lrY9dspfuMXBeTIwPgrMAynFPP-5Lo"
 
 #Teams PowerShell
-$User = "$luServiceAccountUsername"
-$PWord = ConvertTo-SecureString -String $luServiceAccountPassword -AsPlainText -Force
-$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
-Connect-MicrosoftTeams -Credential $credential
+#$User = "$luServiceAccountUsername"
+#$PWord = ConvertTo-SecureString -String $luServiceAccountPassword -AsPlainText -Force
+#$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+#Connect-MicrosoftTeams -Credential $credential
+
+#Teams PowerShell Authentication
+$credentials = Get-AutomationPSCredential -Name 'LoopUp Service Account Credentials'
+Connect-MicrosoftTeams -Credential $credentials 
+
 
 #First/Last Day of Month
 $CURRENTDATE=GET-DATE -Format "MM/dd/yyyy"
